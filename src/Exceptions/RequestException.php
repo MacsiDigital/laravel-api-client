6dd9e7957b2b1f90c3a01a752e103dc85c91afbb
Colin Hall <colin@macsi.co.uk>
2020-05-15 23:55:25 +0100

    Cached Client
    Cache the Laravel Client as it wasnt available in older versions of Laravel

---
+++ src/Exceptions/RequestException.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MacsiDigital\API\Exceptions;
+
+use Exception;
+
+class RequestException extends Exception
+{
+    /**
+     * The response instance.
+     *
+     * @var \Illuminate\Http\Client\Response
+     */
+    public $response;
+
+    /**
+     * Create a new exception instance.
+     *
+     * @param  \Illuminate\Http\Client\Response  $response
+     * @return void
+     */
+    public function __construct(Response $response)
+    {
+        parent::__construct("HTTP request returned status code {$response->status()}.", $response->status());
+
+        $this->response = $response;
+    }
+}
