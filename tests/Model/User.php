ffd3b511e980160d776f7b2682cb2fcff53ab83d
Colin Hall <colin@macsi.co.uk>
2021-01-05 14:00:09 +0000

    Update Tests

--- tests/Model/User.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Tests\Model;
-
-use MacsiDigital\API\Model;
-
-class User extends Model
-{
-}
---
+++ tests/Unit/Test.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MacsiDigital\API\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    /** @test */
+    public function base_test()
+    {
+        $this->assertTrue(true);
+    }
+}
